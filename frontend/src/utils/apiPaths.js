13df51d79ca07a912770c54e8383457e5d5f23e8
nirdeshbhesaniya <you@example.com>
2025-12-14 10:42:26 +0530

    Remove http://localhost:8080/api

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 // src/utils/apiPaths.js
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API = {
   REGISTER: `${API_BASE_URL}/auth/register`,
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'https://interviewai2-0sever.onrender.com/api',
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
