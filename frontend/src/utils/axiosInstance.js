831528c4f6bdc70ce1818e29102f042639ad9589
nirdeshbhesaniya <you@example.com>
2025-12-16 11:41:11 +0530

    ERROR fix to core origin

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://interviewai2-0sever.onrender.com/api',
+  baseURL: 'https://interviewai2-0sever.onrender.com',
   // baseURL: "http://localhost:8080/api",
   headers: {
     'Content-Type': 'application/json',
