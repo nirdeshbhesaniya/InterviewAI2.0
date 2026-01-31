428f768a131a65e676128a877bc256a18d091fb0
nirdeshbhesaniya <you@example.com>
2026-01-31 08:32:31 +0530

    make UI change for responsive

--- frontend/src/components/Chatbot/ChatbotToggle.jsx
+++ frontend/src/components/Chatbot/ChatbotToggle.jsx
@@ -8,7 +8,7 @@ const ChatbotToggle = () => {
     const { isOpen, toggleChatbot, isLoading } = useContext(ChatbotContext);
 
     return (
-        <div className="fixed bottom-6 right-6 z-50">
+        <div className="fixed bottom-4 right-4 sm:bottom-6 sm:right-6 z-50">
             <AnimatePresence>
                 {!isOpen && (
                     <motion.div
